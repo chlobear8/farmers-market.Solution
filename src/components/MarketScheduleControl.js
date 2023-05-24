8bac61ce8cd5e855deba29361ed3f7dba0981859
Chloe ONeil <chloesporri@yahoo.com>
2023-05-23 20:55:26 -0700

    add chooseDay button function

--- src/components/MarketScheduleControl.js
+++ src/components/MarketScheduleControl.js
@@ -1,13 +1,14 @@
 import React from "react";
-import MarketList from "./MarketScheduleList";
-import MarketSchedule from "./MarketSchedule";
+import MarketScheduleList from "./MarketScheduleList";
+//import MarketSchedule from "./MarketSchedule";
 import SeasonalProduce from "./SeasonalProduce";
 
 class MarketScheduleControl extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            scheduleVisibleOnPage: false
+            scheduleVisibleOnPage: false,
+            chooseScheduleDay : null
         };
 
         this.handleClick = this.handleClick.bind(this);
@@ -19,25 +20,36 @@ class MarketScheduleControl extends React.Component {
         }));
     }
 
+    chooseDay = (formInput) => {
+        {console.log(formInput)};
+        this.setState({chooseScheduleDay: formInput});
+    }
+
     render() {
         let currentlyVisibleState = null;
         let buttonText = null;
+        let formInputDay = this.state.chooseScheduleDay;
         if (this.state.scheduleVisibleOnPage) {
             currentlyVisibleState = <SeasonalProduce />;
             buttonText = "Go to market schedule";
         } else {
-            currentlyVisibleState = <MarketSchedule />;
+            currentlyVisibleState = <MarketScheduleList inputDay={formInputDay}/>
             buttonText = "Go to seasonal schedule";
         }
         return (
             <React.Fragment>
+                <button onClick={()=>{this.chooseDay("Monday")}}>Monday</button>
+                <button onClick={()=>{this.chooseDay("Tuesday")}}>Tuesday</button>
+                <button onClick={()=>{this.chooseDay("Wednesday")}}>Wednesday</button>
+                <button onClick={()=>{this.chooseDay("Thursday")}}>Thursday</button>
+                <button onClick={()=>{this.chooseDay("Friday")}}>Friday</button>
+                <button onClick={()=>{this.chooseDay("Saturday")}}>Saturday</button>
+                <button onClick={()=>{this.chooseDay("Sunday")}}>Sunday</button>
                 {currentlyVisibleState}
                 <button onClick={this.handleClick}>{buttonText}</button>
             </React.Fragment>
 
         );
-
-
     }
 }
 
